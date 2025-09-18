e763800a79605b4419d73e8e56ccc016681ccb8c
Efrata Habte <efratahabte1213@gmail.com>
2025-09-18 21:33:56 +0300

    Update Palindrom Number.py (adding Mathematical solution)

--- Palindrom Number.py
+++ Palindrom Number.py
@@ -2,3 +2,20 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         x=str(x)
         return x==x[::-1]
+
+# Mathimatical Approach
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        if x<0:
+            return False
+
+        orginal=x
+        rem=0
+
+        while x>0:
+            digit=x%10
+            rem=rem*10+digit
+            x=x//10
+
+        return orginal==rem

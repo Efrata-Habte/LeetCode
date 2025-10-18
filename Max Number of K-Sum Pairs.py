89b69a475226a3074f4dac8c78067ea1d7606aab
Efrata Habte <efratahabte1213@gmail.com>
2025-10-18 21:36:19 +0300

    Implement maxOperations to find K-sum pairs

---
+++ Max Number of K-Sum Pairs.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        l,r=0,len(nums)-1
+        count=0
+        while l<r:
+            summ=nums[l]+nums[r]
+            if summ==k:
+                count+=1
+                l+=1
+                r-=1
+            elif summ<k:
+                l+=1
+            else:
+                r-=1
+        return count

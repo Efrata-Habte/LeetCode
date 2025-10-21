b53adeb6121cee7208dcffb398e5e2dcd6833e45
Efrata Habte <efratahabte1213@gmail.com>
2025-10-21 09:32:32 +0300

    Implement judgeSquareSum method in Solution class

---
+++ Maximum Matching of Players With Trainers.py
@@ -0,0 +1,13 @@
+class Solution:
+    def judgeSquareSum(self, c: int) -> bool:
+        i=int(math.sqrt(c))
+        k,j=0,i
+        while k<=j:
+            summ=k**2 + j**2
+            if summ==c:
+                return True
+            elif summ<c:
+                k+=1
+            else:
+                j-=1
+        return False

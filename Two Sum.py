b82c6f1b9a8d4d91edb2f4667e9f7a5600e8b34d
Efrata Habte <efratahabte1213@gmail.com>
2025-11-25 10:45:59 +0300

    Update Two Sum.py

--- Two Sum.py
+++ Two Sum.py
@@ -8,3 +8,21 @@ class Solution:
                 return [val[needed],i]
             else:
                 val[nums[i]]=i
+
+# O(Nlogn) solution 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        nums2=list(nums)
+        nums2.sort()
+        n=len(nums)
+        l,r=0,n-1
+        while l<=r:
+            if nums2[l]+nums2[r]>target:
+                r-=1
+            elif nums2[l]+nums2[r]<target:
+                l+=1
+            else:
+                left=nums.index(nums2[l])
+                nums[left]=None
+                return [left,nums.index(nums2[r])]
+        

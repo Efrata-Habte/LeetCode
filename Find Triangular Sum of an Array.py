40ca6fa823ffd47c8a52c2ab4ba046e31c453342
Efrata Habte <efratahabte1213@gmail.com>
2025-11-27 10:55:32 +0300

    Create Find Triangular Sum of an Array.py

---
+++ Find Triangular Sum of an Array.py
@@ -0,0 +1,8 @@
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        while len(nums)>1:
+            newArr=[]
+            for i in range(len(nums)-1):
+                newArr.append((nums[i]+nums[i+1])%10)
+            nums=newArr
+        return nums[0]

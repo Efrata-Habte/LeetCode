99c890a2f32284491b8caba48aa91342d06a0b7c
Efrata Habte <efratahabte1213@gmail.com>
2025-11-27 11:16:41 +0300

    Implement recursive method for triangular sum
    Added a recursive approach to calculate the triangular sum of an array.

--- Find Triangular Sum of an Array.py
+++ Find Triangular Sum of an Array.py
@@ -6,3 +6,12 @@ class Solution:
                 newArr.append((nums[i]+nums[i+1])%10)
             nums=newArr
         return nums[0]
+
+# Recursive approach
+class Solution:
+    def triangularSum(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        
+        newArr=[(nums[i]+nums[i+1])%10 for i in range(len(nums)-1)]
+        return self.triangularSum(newArr)

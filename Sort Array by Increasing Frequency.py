c4b525526666e0cda14944564585c6b1044fb54b
Efrata Habte <efratahabte1213@gmail.com>
2025-12-10 23:48:42 +0300

    Implement frequency sort for an array

---
+++ Sort Array by Increasing Frequency.py
@@ -0,0 +1,13 @@
+class Solution:
+    def frequencySort(self, nums: List[int]) -> List[int]:
+        nums.sort(reverse=True)
+        count=Counter(nums)
+        count=OrderedDict(sorted(count.items(),key=lambda x:x[1], reverse=True))
+        ans=[]
+
+        for i in count:
+            while count[i] >0:
+                ans.append(i)
+        return ans
+
+        

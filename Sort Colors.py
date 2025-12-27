5886595b773d00ca266e97f8c92d22f0572df52f
Efrata Habte <efratahabte1213@gmail.com>
2025-12-27 11:31:44 +0300

    Update Sort Colors.py

--- Sort Colors.py
+++ Sort Colors.py
@@ -10,3 +10,19 @@ class Solution:
                     temp=nums[i]
                     nums[i]=nums[j]
                     nums[j]=temp
+# efficient approach
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        count=Counter(nums)
+        print (count)
+        n=len(nums)
+        j=0
+        for i in range (3):
+            k=0
+            while k<count[i] and j<n:
+                nums[j]=i
+                j+=1
+                k+=1
